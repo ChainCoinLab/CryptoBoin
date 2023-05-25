c1c41833fa3f279a33fab67a2b7a4d66c9744891
taotie <taotiejiuzhou@163.com>
2023-05-25 10:26:55 +0800

    Initial Commit

---
+++ crypto tracker/crypto_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  crypto_trackerApp.swift
+//  crypto tracker
+//
+//  Created by fcihpy on 2023/5/25.
+//
+
+import SwiftUI
+
+@main
+struct crypto_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
